51988613287ddc4500b0450e294344da92782d5c
aMaverickid <112038720+aMaverickid@users.noreply.github.com>
2025-02-20 14:27:28 +0000

    Deploying to gh-pages from @ aMaverickid/amaverickid.github.io@c8869107710eec4f3b3f05447519d3314d8629b8 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -9,7 +9,14 @@ ninja.data = [{
     handler: () => {
       window.location.href = "/";
     },
-  },{id: "news-a-simple-inline-announcement",
+  },{id: "nav-cv",
+          title: "cv",
+          description: "This is a description of the page. You can modify it in &#39;_pages/cv.md&#39;. You can also change or remove the top pdf download button.",
+          section: "Navigation",
+          handler: () => {
+            window.location.href = "/cv/";
+          },
+        },{id: "news-a-simple-inline-announcement",
           title: 'A simple inline announcement.',
           description: "",
           section: "News",},{id: "news-a-long-announcement-with-details",
